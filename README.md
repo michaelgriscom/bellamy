cb38a021563f5e7e103cc6eb9794f08b1dcf9da2
Michael Griscom <michaelgriscom@gmail.com>
2022-03-29 19:17:20 -0700

    cleanup

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # bellamy
  
+## Development
+- Open in VSCode
+- Launch Calva REPL
+- Navigate to http://localhost:8280
--- src/bellamy/views.cljs
+++ src/bellamy/views.cljs
@@ -1,12 +1,9 @@
 (ns bellamy.views
   (:require
    [re-frame.core :as re-frame]
-   [bellamy.subs :as subs]
-   ))
+   [bellamy.subs :as subs]))
 
 (defn main-panel []
-  (let [name (re-frame/subscribe [::subs/name])]
-    [:div
-     [:h1
-      "Hello world"]
-     ]))
+  [:div
+   [:h1
+    "Hello world"]])
