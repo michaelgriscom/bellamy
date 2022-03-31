4f9aadfe82608c1fb1c6045e85a96f6d4675a477
Michael Griscom <michaelgriscom@gmail.com>
2022-03-30 21:05:52 -0700

    cleanup

--- test/core_test.cljs
+++ test/core_test.cljs
@@ -5,5 +5,13 @@
 (deftest basic-rewards
   (testing "basic rewards"
     (let [budget {:groceries 100, :shopping 200}
-          card {:groceries 0.02, :shopping 0.02}]
-      (is (= {:groceries 2, :shopping 4} (calc/calculate-rewards budget card))))))
+          card {:groceries 0.02, :shopping 0.03}]
+      (is (= {:groceries 2, :shopping 6}
+             (calc/calculate-rewards budget card))))))
+
+(deftest basic-rewards
+  (testing "basic rewards"
+    (let [budget {:groceries 100, :shopping 200}
+          card {:shopping 0.01}]
+      (is (= {:groceries 0, :shopping 2}
+             (calc/calculate-rewards budget card))))))
